0ddc3c6a4c95b9aaa7ecc4d9c8ae110f463ba750
Kacper Najder <40029774+kacpernajder@users.noreply.github.com>
2024-08-08 14:52:43 +0100

    Create solutions.php

---
+++ solutions.php
@@ -0,0 +1,22 @@
+add_action('template_redirect', 'preserve_utms');
+
+function preserve_utms() {
+    if (!is_cart() && !is_checkout()) {
+        return;
+    }
+
+    $utm_params = array('utm_medium', 'utm_source', 'utm_campaign', 'utm_id', 'utm_content', 'ref');
+    $current_url = add_query_arg(null, null);
+    $new_url = $current_url;
+
+    foreach ($utm_params as $param) {
+        if (isset($_GET[$param])) {
+            $new_url = add_query_arg($param, $_GET[$param], $new_url);
+        }
+    }
+
+    if ($current_url !== $new_url) {
+        wp_safe_redirect($new_url);
+        exit;
+    }
+}
