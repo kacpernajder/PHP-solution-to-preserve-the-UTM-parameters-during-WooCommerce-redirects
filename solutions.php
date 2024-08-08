6217aa69a735b397229da0cb12e46e701bb9a800
Kacper Najder <40029774+kacpernajder@users.noreply.github.com>
2024-08-08 15:40:26 +0100

    Update solutions.php

--- solutions.php
+++ solutions.php
@@ -1,24 +1,22 @@
 Add the following code to theme's functions.php file:
 
-add_action('template_redirect', 'preserve_utms');
-
-function preserve_utms() {
-    if (!is_cart() && !is_checkout()) {
-        return;
-    }
-
+function store_utm_parameters() {
     $utm_params = array('utm_medium', 'utm_source', 'utm_campaign', 'utm_id', 'utm_content', 'ref');
-    $current_url = add_query_arg(null, null);
-    $new_url = $current_url;
-
     foreach ($utm_params as $param) {
         if (isset($_GET[$param])) {
-            $new_url = add_query_arg($param, $_GET[$param], $new_url);
+            setcookie($param, $_GET[$param], time() + 3600, '/');
         }
     }
-
-    if ($current_url !== $new_url) {
-        wp_safe_redirect($new_url);
-        exit;
-    }
 }
+add_action('init', 'store_utm_parameters');
+
+function append_utm_parameters_to_checkout($url) {
+    $utm_params = array('utm_medium', 'utm_source', 'utm_campaign', 'utm_id', 'utm_content', 'ref');
+    foreach ($utm_params as $param) {
+        if (isset($_COOKIE[$param])) {
+            $url = add_query_arg($param, $_COOKIE[$param], $url);
+        }
+    }
+    return $url;
+}
+add_filter('woocommerce_get_checkout_url', 'append_utm_parameters_to_checkout');
